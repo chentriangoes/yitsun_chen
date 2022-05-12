6e83125c8dee2f660a9f1ed0c64c7145f54aa710
chentriangoes <chentriangoes@gmail.com>
2022-05-12 13:03:16 +1000

    12th commit_About Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ import Homeimage from '../images/Grad.jpg'
 export default function Home() {
   return (
         <div className="background">
-            <img className="home-image" src={Homeimage} alt="Yi-Tsun's grad taken with the former Miniter for Foreign Affairs, Julie Bishoon on the stage of graduation" />
+            <img className="home-image" src={Homeimage} alt="Yi-Tsun's grad taken with the former Miniter for Foreign Affairs, 
+            Julie Bishoon on the stage of graduation" />
             <div className="home-text">
                 <h1 className="home-drchen">Dr Yi-Tsun Chen</h1>
                 <p>I need to add something here</p>
