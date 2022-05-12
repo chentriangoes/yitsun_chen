6bd5e3030f3cc68abbeca9ee3d0b3276f5bd3569
chentriangoes <chentriangoes@gmail.com>
2022-05-12 12:34:52 +1000

    10th commit_Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 //type rfc to automatically import
 import React from 'react'
+import "./Home.css"
+import Homeimage from '../images/Grad.jpg'
 
 export default function Home() {
   return (
-    <div>Home 首頁</div>
+        <div className="background">
+            <img className="home-image" src={Homeimage} alt="Yi-Tsun's grad taken with the former Miniter for Foreign Affairs, Julie Bishoon on the stage of graduation" />
+            <div className="home-text">
+                <h1 className="home-drchen">Dr Yi-Tsun Chen</h1>
+                <p>I need to add something here</p>
+            </div>
+        </div>
   )
 }
+
+
+
