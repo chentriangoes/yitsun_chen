3271ba1cfd4c5cb1dd7d799ac84668f62d62d52f
chentriangoes <chentriangoes@gmail.com>
2022-05-12 13:11:52 +1000

    13th commit_Work

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,35 +3,38 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-        <article>
-          <p>
-            A Ph.D. graduate from the medical school of the Australian National University with expertise in conducting health research from an interdisciplinary lens. Experienced in quality customer services and working under multiple supervisors across departments for research management ranging from proposals evaluation, grants coordination, quantitative and qualitative data analytics, and research strategies planning. Currently trained at a coding bootcamp in the University of Western Australia towards a certified full-stack web developer with advanced digital literacy in UI/UX design, managing, analysing, associating, showcasing, and securing data. Also enrolled in the program of Master of Applied Buddhist Studies at the Nan Tien Institute, Wollongong, to develop capacity of adopting critical and practical approaches to inform public health policies and programs. Looking forwards to contributing to this position, the team, and the community by leveraging skills in consolidating various innovative techniques of the web development, research management, data analytics, and anthropological studies on health.
-          </p>
-          <h3> Research Interest </h3>
-          <p>
-            Vulnerability, Social Suffering, Everyday Struggles, Health Policy and Practice, Gender/Sexualities Studies, Global Health, Science and Technology Studies, Applied Buddhist Studies, Digital Health
-          </p>
-          <h3>Education</h3>
-          <ul>
-            <li>PhD (Medical School, The Australian National University)</li>
-            <li>MPH (College of Medicine, Biology and Environment, The Australian National University)</li>
-            <li>MSc (Graduate Institute of Anatomy and Cell Biology, National Taiwan University)</li>
-            <li>BSc (Department of Life Science, National Chung Hsing University)</li>
-          </ul>
-          <h3>Achievements</h3>
-          <ul>
-            <li><a href="https://science.anu.edu.au/alumni/you-see-it/winners" rel="noreferrer" target="_blank">2021 Winner of “From across the global,” ANU College of Science ‘As You See it’ Photography Competition</a></li>
-            <li><a href="https://reurl.cc/GmEWEp" rel="noreferrer" target="_blank">2020 ANU Gender Institute PRIZE (PhD Thesis) For Excellence in Gender and Sexuality research </a></li>
-            <li>2017-2019 ANU HDR Fee Remission Merit Scholarship</li>
-          </ul>
-          <h3>Web Developments (Deployed Page and Please Find Source Codes at "Works")</h3>
-          <ul>
-            <li><a href="https://project-2-dw.herokuapp.com/" rel="noreferrer" target="_blank">Junglr.</a></li>
-            <li><a href="https://chentriangoes.github.io/Cocktail_Diversifier/" rel="noreferrer" target="_blank">Cocktail Diversifier</a></li>
-            <li><a href="https://mighty-woodland-73767.herokuapp.com/notes" rel="noreferrer" target="_blank">Note Taker</a></li>
-            <li><a href="https://chentriangoes.github.io/rainorsun/" rel="noreferrer" target="_blank">Weather Dashboard</a></li>
-          </ul>  
-        </article>
+<section id="aboutme">
+
+  <article>
+    <p>
+      A Ph.D. graduate from the medical school of the Australian National University with expertise in conducting health research from an interdisciplinary lens. Experienced in quality customer services and working under multiple supervisors across departments for research management ranging from proposals evaluation, grants coordination, quantitative and qualitative data analytics, and research strategies planning. Currently trained at a coding bootcamp in the University of Western Australia towards a certified full-stack web developer with advanced digital literacy in UI/UX design, managing, analysing, associating, showcasing, and securing data. Also enrolled in the program of Master of Applied Buddhist Studies at the Nan Tien Institute, Wollongong, to develop capacity of adopting critical and practical approaches to inform public health policies and programs. Looking forwards to contributing to this position, the team, and the community by leveraging skills in consolidating various innovative techniques of the web development, research management, data analytics, and anthropological studies on health.
+    </p>
+    <h3> Research Interest </h3>
+    <p>
+      Vulnerability, Social Suffering, Everyday Struggles, Health Policy and Practice, Gender/Sexualities Studies, Global Health, Science and Technology Studies, Applied Buddhist Studies, Digital Health
+    </p>
+    <h3>Education</h3>
+    <ul>
+      <li>PhD (Medical School, The Australian National University)</li>
+      <li>MPH (College of Medicine, Biology and Environment, The Australian National University)</li>
+      <li>MSc (Graduate Institute of Anatomy and Cell Biology, National Taiwan University)</li>
+      <li>BSc (Department of Life Science, National Chung Hsing University)</li>
+    </ul>
+    <h3>Achievements</h3>
+    <ul>
+      <li><a href="https://science.anu.edu.au/alumni/you-see-it/winners" rel="noreferrer" target="_blank">2021 Winner of “From across the global,” ANU College of Science ‘As You See it’ Photography Competition</a></li>
+      <li><a href="https://reurl.cc/GmEWEp" rel="noreferrer" target="_blank">2020 ANU Gender Institute PRIZE (PhD Thesis) For Excellence in Gender and Sexuality research </a></li>
+      <li>2017-2019 ANU HDR Fee Remission Merit Scholarship</li>
+    </ul>
+    <h3>Web Developments (Deployed Page and Please Find Source Codes at "Works")</h3>
+    <ul>
+      <li><a href="https://project-2-dw.herokuapp.com/" rel="noreferrer" target="_blank">Junglr.</a></li>
+      <li><a href="https://chentriangoes.github.io/Cocktail_Diversifier/" rel="noreferrer" target="_blank">Cocktail Diversifier</a></li>
+      <li><a href="https://mighty-woodland-73767.herokuapp.com/notes" rel="noreferrer" target="_blank">Note Taker</a></li>
+      <li><a href="https://chentriangoes.github.io/rainorsun/" rel="noreferrer" target="_blank">Weather Dashboard</a></li>
+    </ul>  
+  </article>
+</section>
       </div>
   )
 }
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Work() {
   return (
-    <div>Work 作品</div>
+    <div>
+      <section id="youtube">
+  <section>
+    <iframe width={560} height={315} src="https://www.youtube.com/embed/videoseries?list=PLqWpYzaTLaBiwCfKWAx9BhYSTnJaTBf5o" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+               picture-in-picture" allowFullScreen />
+    <figcaption>Love Narratives: One Five and A Ten, Persons with HIV/AIDS Rights Advocacy Association of Taiwan (PRAATW)
+    </figcaption></section>
+  <section>
+    <iframe width={560} height={315} src="https://www.youtube.com/embed/videoseries?list=PLUusY-C_9sdfXNOHvJQ_oYvjCiR1A5S94" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+               picture-in-picture" allowFullScreen />
+    <figcaption> The Way (Chandler Huang), Taiwan International Queer Festival 2019</figcaption>
+  </section>
+</section>
+    </div>
   )
 }
