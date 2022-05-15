d74cf7c72496799e0bdae675efdb5db41fc44c5f
chentriangoes <chentriangoes@gmail.com>
2022-05-15 21:34:58 +1000

    25th commit_Work

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import './Work.css'
 
 export default function Work() {
   return (
-    <div>
-      <section id="youtube">
-  <section>
-    <iframe width={560} height={315} src="https://www.youtube.com/embed/videoseries?list=PLqWpYzaTLaBiwCfKWAx9BhYSTnJaTBf5o" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+    <div className='work'>
+      <section className='youtube'>
+        <section>
+          <h3>Love Narratives: One Five and A Ten</h3>
+          <iframe width={560} height={315} src="https://www.youtube.com/embed/videoseries?list=PLqWpYzaTLaBiwCfKWAx9BhYSTnJaTBf5o" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
                picture-in-picture" allowFullScreen />
-    <figcaption>Love Narratives: One Five and A Ten, Persons with HIV/AIDS Rights Advocacy Association of Taiwan (PRAATW)
-    </figcaption></section>
-  <section>
-    <iframe width={560} height={315} src="https://www.youtube.com/embed/videoseries?list=PLUusY-C_9sdfXNOHvJQ_oYvjCiR1A5S94" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+              <figcaption>Persons with HIV/AIDS Rights Advocacy Association of Taiwan (PRAATW)</figcaption>
+        </section>
+
+        <section>
+          <h3>The Way</h3>
+          <iframe width={560} height={315} src="https://www.youtube.com/embed/videoseries?list=PLUusY-C_9sdfXNOHvJQ_oYvjCiR1A5S94" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
                picture-in-picture" allowFullScreen />
-    <figcaption> The Way (Chandler Huang), Taiwan International Queer Festival 2019</figcaption>
-  </section>
-</section>
+               <figcaption>Chandler Huang, Taiwan International Queer Festival 2019</figcaption>
+        </section>
+     </section>
     </div>
   )
 }
