d07de73bde42051884f8714e4aeb73d5aa015d5e
chentriangoes <chentriangoes@gmail.com>
2022-08-04 15:15:36 +1000

    Updates

---
+++ precache-manifest.61fabfc5b74aa08d052ec5a64f662148.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "5213761d851174c28131",
+    "url": "/yitsun_chen/static/css/main.43e9cccf.chunk.css"
+  },
+  {
+    "revision": "5213761d851174c28131",
+    "url": "/yitsun_chen/static/js/main.5213761d.chunk.js"
+  },
+  {
+    "revision": "9b47298707f365d7f274",
+    "url": "/yitsun_chen/static/js/1.9b472987.chunk.js"
+  },
+  {
+    "revision": "75bd317bccb15b6da0d5",
+    "url": "/yitsun_chen/static/js/2.75bd317b.chunk.js"
+  },
+  {
+    "revision": "af069399968e2766cd5f",
+    "url": "/yitsun_chen/static/js/runtime~main.af069399.js"
+  },
+  {
+    "revision": "ae4f7a9017f811f2072cac775d6de5c4",
+    "url": "/yitsun_chen/static/media/knight_03.ae4f7a90.jpg"
+  },
+  {
+    "revision": "339dc42ab38bd67d2f16a7e6bff304e2",
+    "url": "/yitsun_chen/static/media/logo.339dc42a.jpg"
+  },
+  {
+    "revision": "8934e316ebb8528500dbf4eca6c76298",
+    "url": "/yitsun_chen/static/media/yitsun01.8934e316.gif"
+  },
+  {
+    "revision": "afc20baf9231b7aad687ecc61cbd445a",
+    "url": "/yitsun_chen/static/media/yitsun02.afc20baf.gif"
+  },
+  {
+    "revision": "244b092626ef773b0bccfe5f2382a8f7",
+    "url": "/yitsun_chen/static/media/yitsun03.244b0926.gif"
+  },
+  {
+    "revision": "fea1d9584d24c4ef668040b8f1a6c8de",
+    "url": "/yitsun_chen/static/media/yitsun04.fea1d958.gif"
+  },
+  {
+    "revision": "c68e0ba8a1802301c6f62bdde50d7909",
+    "url": "/yitsun_chen/static/media/Lanyu01.c68e0ba8.jpg"
+  },
+  {
+    "revision": "ef16687c0451c45d7e2a6d754b0b68f9",
+    "url": "/yitsun_chen/static/media/Lanyu02.ef16687c.jpeg"
+  },
+  {
+    "revision": "9fa671e560d7a1bcec7029c4fd42561a",
+    "url": "/yitsun_chen/static/media/knight_01.9fa671e5.jpg"
+  },
+  {
+    "revision": "284e924cdc9609725fabcdcdafa78852",
+    "url": "/yitsun_chen/static/media/knight_02.284e924c.jpg"
+  },
+  {
+    "revision": "71a02a76958b42f34936a6ad0e15e71f",
+    "url": "/yitsun_chen/static/media/Resume_Yi-Tsun.71a02a76.pdf"
+  },
+  {
+    "revision": "41febaede74c240ad831d0729f17d7b5",
+    "url": "/yitsun_chen/index.html"
+  }
+];
